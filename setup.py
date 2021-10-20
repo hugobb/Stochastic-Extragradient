d73461fafee27a0ee426b804684121e6393acaba
Hugo Berard <hberard@fb.com>
2021-10-20 12:34:51 -0400

    Updating setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hamiltonian",
+    name="gameopt",
     version="0.1",
     author="Hugo Berard",
     author_email="berard.hugo@gmail.com",
@@ -20,8 +20,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "hamiltonian"},
-    packages=setuptools.find_packages(where="hamiltonian"),
+    package_dir={"": "gameopt"},
+    packages=setuptools.find_packages(where="gameopt"),
     python_requires=">=3.6",
     install_requires=[
         'tqdm',
